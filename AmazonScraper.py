34d89a878edac58458a62d8886df48695d0c2bb6
wli0 <144973371+wli0@users.noreply.github.com>
2024-08-18 22:50:58 -0400

    Used a .env file to safely enter email and password

--- AmazonScraper.py
+++ AmazonScraper.py
@@ -4,6 +4,15 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import smtplib
+import os
+from dotenv import load_dotenv
+import time 
+
+
+load_dotenv()
+
+EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
+APP_PASSWORD = os.getenv('APP_PASSWORD')
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")  
@@ -16,7 +25,7 @@ options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 # URL
-url = 'https://www.amazon.com/Dymatize-Protein-Isolate-Gourmet-Vanilla/dp/B00G6QHYZ6?pd_rd_w=QLnO4&content-id=amzn1.sym.1c5f8f79-ee2f-4fb3-a1b5-3d8384cdfaf3&pf_rd_p=1c5f8f79-ee2f-4fb3-a1b5-3d8384cdfaf3&pf_rd_r=390MWH2T8EQV1DRCEQ5Z&pd_rd_wg=cQUrm&pd_rd_r=55e6860d-f432-4e8e-b439-6fca56853761&pd_rd_i=B00G6QHYZ6&psc=1&ref_=pd_bap_d_grid_rp_0_1_ec_scp_pd_nav_hcs_rp_1_t'
+url = input("Enter the amazon product page URL to start:")
 driver.get(url)
 
 # Wait
@@ -27,6 +36,7 @@ page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'lxml')
 
 TARGET_PRICE = float(input("Enter the target price: "))
+EMAIL = input("Enter your email address: ")
 
 def check_info():
     try:
@@ -57,22 +67,22 @@ def check_info():
     except Exception as e:
         print(f"An error occurred: {e}")
 
-def send_email(product_name, product_price, url):
+def send_email(product_name, product_price, url, EMAIL):
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
         server.ehlo()
 
-        server.login('williamli0610@gmail.com', 'qvbi vmsm ajne nhzm')
+        server.login(EMAIL_ADDRESS, APP_PASSWORD)
 
         subject = f"Price Drop Alert: {product_name}"
         body = f"The price for {product_name} has dropped to ${product_price}! You can use this link to head to the product page: {url}"
         msg = f"Subject: {subject}\n\n{body}"
 
         server.sendmail(
-            'williamli0610@gmail.com',
-            'williamli0610@gmail.com',
+            EMAIL_ADDRESS,
+            EMAIL,
             msg
         )
         server.quit()
@@ -81,5 +91,9 @@ def send_email(product_name, product_price, url):
     except Exception as e:
         print(f"Failed to send email: {e}")
 
-check_info()
+while True:
+    if check_info():
+        break
+
+    time.sleep(86400)
 driver.quit()
